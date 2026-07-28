d02926dc5d5e503c5d485eadacedde2d015b2969
Eli Array Minkoff <eli@planetminkoff.com>
2026-07-28 15:15:41 -0400

    Add header with some helpful trivial typedefs

---
+++ include/typenames.h
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Eli Array Minkoff
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+#ifndef UCLP_TYPENAMES
+#define UCLP_TYPENAMES
+
+typedef unsigned int uint;
+typedef unsigned char uchar;
+typedef signed char schar;
+#endif
