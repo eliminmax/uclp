2e4fd131e652220f853dcae10f07f260f90d56df
Eli Array Minkoff <eli@planetminkoff.com>
2026-07-28 15:10:51 -0400

    Improve hashmap implementation
    Using my knowlege gained from Crafting Interpreters, I improved the
    implementation, primarily by adding headstones

--- src/hash.c
+++ src/hash.c
@@ -8,13 +8,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "mem/checked.h"
 #include "hash.h"
+#include "safety.h"
+#include "mem/checked.h"
 #include "sized_string.h"
 
 #define STRING_DUP(s) \
-    ((String){.len = s.len, \
-              .text = memcpy(checked_malloc(s.len), s.text, s.len)})
+    ((String){ \
+        .len = s.len, .text = memcpy(checked_malloc(s.len), s.text, s.len) \
+    })
 #define STRING_EQ(a, b) \
     ((a.len == b.len) ? (memcmp(a.text, b.text, a.len) == 0) : false)
 
@@ -33,36 +35,52 @@ uint64_t hash_string(const String s) {
 
 struct ht_entry {
     String id;
+    uint64_t hash;
     // the data pointer for this entry
-    void *_Nonnull data;
-    bool occupied;
+    void *_Nullable data;
+
+    enum : int8_t {
+        HT_ENTRY_TOMBSTONE = -1,
+        HT_ENTRY_EMPTY = 0,
+        HT_ENTRY_OCCUPIED = 1,
+    } status;
 };
 
 struct hash_table {
-    size_t len;
-    size_t cap;
-    [[clang::counted_by(cap)]] struct ht_entry *_Nonnull entries;
+    size_t count;
+    size_t capacity;
+    [[clang::counted_by(capacity)]] struct ht_entry *_Nonnull entries;
 };
+
+#ifdef NDEBUG
+[[gnu::const]]
+#endif
+[[gnu::artificial]]
+static inline size_t hash_index(uint64_t hash, size_t ht_capacity) {
+    REQUIRE(ht_capacity > 0);
+    REQUIRE((ht_capacity & (ht_capacity - 1)) == 0);
+    return hash & (ht_capacity - 1);
+}
 
 HashTable ht_create() {
     struct hash_table *table = checked_malloc(sizeof(struct hash_table));
-    table->len = 0;
-    table->cap = 8;
+    table->count = 0;
+    table->capacity = 8;
     table->entries = checked_calloc(sizeof(struct ht_entry), 8);
-    for (int i = 0; i < 8; ++i) table->entries[i].occupied = false;
+    for (int i = 0; i < 8; ++i) table->entries[i].status = HT_ENTRY_EMPTY;
 
     return table;
 }
 
 void ht_clear(struct hash_table *table, HTEntryCleanupFn callback) {
-    for (size_t i = 0; i < table->cap; ++i) {
-        if (table->entries[i].occupied) {
+    for (size_t i = 0; i < table->capacity; ++i) {
+        if (table->entries[i].status) {
             if (callback) callback(table->entries[i].data);
             free(table->entries[i].id.text);
-            table->entries[i].occupied = false;
+            table->entries[i].status = false;
         }
     }
-    table->len = 0;
+    table->count = 0;
 }
 
 void ht_cleanup(struct hash_table *table, HTEntryCleanupFn callback) {
@@ -74,34 +92,34 @@ void ht_cleanup(struct hash_table *table, HTEntryCleanupFn callback) {
 void *_Nullable ht_lookup(
     struct hash_table *const _Nonnull table, const String id
 ) {
-    size_t index = hash_string(id) % table->cap;
-    while (table->entries[index].occupied) {
+    size_t index = hash_index(hash_string(id), table->capacity);
+    while (table->entries[index].status) {
         if (STRING_EQ(id, table->entries[index].id)) {
             return table->entries[index].data;
         }
         index++;
-        index %= table->cap;
+        index %= table->capacity;
     }
 
     return NULL;
 }
 
 static void ht_extend_cap(struct hash_table *_Nonnull table) {
-    struct ht_entry *new_alloc = checked_calloc(table->cap, 2);
+    struct ht_entry *new_alloc = checked_calloc(table->capacity, 2);
     struct ht_entry *old_alloc = table->entries;
 
-    size_t new_cap = table->cap * 2;
-    for (size_t i = 0; i < table->cap; ++i) {
-        if (old_alloc[i].occupied) {
+    size_t new_cap = table->capacity * 2;
+    for (size_t i = 0; i < table->capacity; ++i) {
+        if (old_alloc[i].status) {
             size_t index = hash_string(old_alloc[i].id) % new_cap;
-            while (new_alloc[index].occupied) {
+            while (new_alloc[index].status) {
                 index++;
                 index %= new_cap;
             }
             memcpy(&new_alloc[index], &old_alloc[i], sizeof(struct ht_entry));
         }
     }
-    table->cap = new_cap;
+    table->capacity = new_cap;
     table->entries = new_alloc;
     free(old_alloc);
 }
@@ -109,30 +127,41 @@ static void ht_extend_cap(struct hash_table *_Nonnull table) {
 void *_Nullable ht_set(
     struct hash_table *_Nonnull table, const String id, void *_Nonnull data
 ) {
-    size_t index = hash_string(id) % table->cap;
-
-    while (table->entries[index].occupied) {
-        if (STRING_EQ(id, table->entries[index].id)) {
-            void *olddata = table->entries[index].data;
-            table->entries[index].data = data;
+    size_t index = hash_index(hash_string(id), table->capacity);
+    struct ht_entry *entry;
+    struct ht_entry *tombstone = NULL;
+    while (entry = &table->entries[index], entry->status) {
+        if (entry->status == HT_ENTRY_TOMBSTONE) {
+            if (!tombstone) tombstone = entry;
+        } else if (entry->status && STRING_EQ(id, entry->id)) {
+            void *olddata = entry->data;
+            entry->data = data;
             return olddata;
         }
         index++;
-        index %= table->cap;
+        index %= table->capacity;
     }
 
-    ++table->len;
-    if (table->len > table->cap / 2) {
-        ht_extend_cap(table);
-        index = hash_string(id) % table->cap;
-        while (table->entries[index].occupied) {
-            // already know that it's not already in the table, so no need to
-            // check.
-            index++;
-            index %= table->cap;
+    if (tombstone) {
+        entry = tombstone;
+    } else {
+        ++table->count;
+        if (table->count > table->capacity * 0.75) {
+            ht_extend_cap(table);
+            index = hash_index(hash_string(id), table->capacity);
+            while (table->entries[index].status == HT_ENTRY_OCCUPIED) {
+                // already know that it's not already in the table, so no need to
+                // check.
+                index++;
+                index %= table->capacity;
+            }
         }
+
+        entry = &table->entries[index];
     }
-    table->entries[index].occupied = true;
+
+
+    table->entries[index].status = HT_ENTRY_OCCUPIED;
     table->entries[index].data = data;
     table->entries[index].id = STRING_DUP(id);
     return NULL;
